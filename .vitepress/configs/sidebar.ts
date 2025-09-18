3875d20176334711ae866d00879871c2ca3b5961
fcbyk <731240932@qq.com>
2025-09-18 23:29:51 +0800

    chore: 修复错误链接，改变目录结构

--- .vitepress/configs/sidebar.ts
+++ .vitepress/configs/sidebar.ts
@@ -3,10 +3,15 @@ export default [
         text: '理论知识',
         collapsed: true,
         items: [
-            {text:"C++概述",link:"/qa/01"},
-            {text:"环境配置",link:"/qa/02"},
-            {text:"CMake构建工具",link:"/qa/03"},
-            {text:"CLion常见问题 ",link:"/qa/04"},
+            {
+                text: "起步",
+                items:[
+                    {text:"C++概述",link:"/qa/01"},
+                    {text:"环境配置",link:"/qa/02"},
+                    {text:"CMake构建工具",link:"/qa/03"},
+                    {text:"CLion常见问题 ",link:"/qa/04"}
+                ]
+            },
             {text:"递归",link:"/qa/10"},
             {text:"指针",link:"/qa/11"},
         ]
