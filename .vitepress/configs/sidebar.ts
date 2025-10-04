63b1239b66e1b90e1c9916fa475b2b3ea74a563b
yoki <731240932@qq.com>
2025-10-04 10:46:07 +0800

    docs: 函数QA

--- .vitepress/configs/sidebar.ts
+++ .vitepress/configs/sidebar.ts
@@ -8,6 +8,7 @@ export default [
             {text:"数据类型",link:"/qa/06"},
             {text:"运算符",link:"/qa/operator/"},
             {text:"控制语句",link:"/qa/control/"},
+            {text:"函数",link:"/qa/04"},
             {text:"递归",link:"/qa/10"},
             {text:"指针",link:"/qa/11"},
         ]
